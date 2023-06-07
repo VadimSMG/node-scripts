47239e9d4932f06ee2cdf1b1d6543a769a45d2a0
SandRat <vadimsmg@gmail.com>
2023-06-07 18:24:51 +0300

    Edit nolus-install script for installing GO v1.20.4 (previos was v1.96)

--- nolus-install.sh
+++ nolus-install.sh
@@ -4,9 +4,9 @@ apt -q update
 apt -qy install curl git jq lz4 build-essential
 apt -qy upgrade
 
-#Install GO v1.19.6
+#Install GO v1.20.4
 rm -rf /usr/local/go
-curl -Ls https://go.dev/dl/go1.19.6.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+curl -Ls https://go.dev/dl/go1.20.4.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
 eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
 eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 
