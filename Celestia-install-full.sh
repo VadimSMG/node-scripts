836b976e887d01d17da69ce68bdc4bf66b527da8
VadimSMG <106158663+VadimSMG@users.noreply.github.com>
2023-07-14 20:06:20 +0300

    Add colored echo

--- Celestia-install-full.sh
+++ Celestia-install-full.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Setup dependencies"
+echo -e "\033[0;34mSetup dependencies"
 sudo apt update && sudo apt upgrade -y
 sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu -y
 
-echo "Installing Golang"
+echo -e "\033[0;34mInstalling Golang"
 ver="1.20.2"
 cd $HOME
 wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
@@ -12,11 +12,11 @@ sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
 rm "go$ver.linux-amd64.tar.gz"
 
-echo "Export PATH variable"
+echo -e "\033[0;34mExport PATH variable"
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
-echo "Install celestia-app"
+echo -e "\033[0;34mInstall celestia-app"
 cd $HOME
 rm -rf celestia-app
 git clone https://github.com/celestiaorg/celestia-app.git
@@ -25,22 +25,22 @@ APP_VERSION=v1.0.0-rc9
 git checkout tags/$APP_VERSION -b $APP_VERSION
 make install
 
-echo "Setup P2P networks"
+echo -e "\033[0;34mSetup P2P networks"
 cd $HOME
 rm -rf networks
 git clone https://github.com/celestiaorg/networks.git
 
-echo "Input node name:"
+echo -e "\033[0;34mInput node name:"
 read NODENAME
 
-echo "Node initing"
+echo -e "\033[0;34mNode initing"
 celestia-appd init "$NODENAME" --chain-id mocha-3
 cp $HOME/networks/mocha/genesis.json $HOME/.celestia-app/config
 PERSISTENT_PEERS=$(curl -sL https://raw.githubusercontent.com/celestiaorg/networks/master/mocha/peers.txt | tr -d '\n')
 echo $PERSISTENT_PEERS
 sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PERSISTENT_PEERS\"/" $HOME/.celestia-app/config/config.toml
 
-echo "Configurating pruning"
+echo -e "\033[0;34mConfigurating pruning"
 PRUNING="custom"
 PRUNING_KEEP_RECENT="100"
 PRUNING_INTERVAL="10"
@@ -51,10 +51,10 @@ sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \
 \"$PRUNING_INTERVAL\"/" $HOME/.celestia-app/config/app.toml
 
-echo "Network reset"
+echo -e "\033[0;34mNetwork reset"
 celestia-appd tendermint unsafe-reset-all --home $HOME/.celestia-app
 
-echo "Quick node syncing"
+echo -e "\033[0;34mQuick node syncing"
 cd $HOME
 rm -rf ~/.celestia-app/data
 mkdir -p ~/.celestia-app/data
@@ -63,10 +63,10 @@ SNAP_NAME=$(curl -s https://snaps.qubelabs.io/celestia/ | \
 wget -O - https://snaps.qubelabs.io/celestia/${SNAP_NAME} | tar xf - \
     -C ~/.celestia-app/data/
 
-echo "RPC endpoint configuration"
+echo -e "\033[0;34mRPC endpoint configuration"
 EXTERNAL_ADDRESS=$(wget -qO- eth0.me)
 sed -i.bak -e "s/^external_address = \"\"/external_address = \"$EXTERNAL_ADDRESS:26656\"/" $HOME/.celestia-app/config/config.toml
 sed -i 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.celestia-app/config/config.toml
 
-echo "Starting celestia app"
+echo -e "\033[0;34mStarting celestia app"
 celestia-appd start
