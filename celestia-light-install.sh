5a788f5c90032a19a495c64dd01bf9ec767c3659
VadimSMG <106158663+VadimSMG@users.noreply.github.com>
2023-07-23 07:28:10 +0300

    Update celestia-light-install.sh

--- celestia-light-install.sh
+++ celestia-light-install.sh
@@ -6,23 +6,23 @@ sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential
 git make ncdu -y
 
 echo "3. GOLANG INSTALL"
-ver="1.19.1" 
-cd $HOME 
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" 
-sudo rm -rf /usr/local/go 
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" 
+ver="1.20.2"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
 rm "go$ver.linux-amd64.tar.gz"
 
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 echo "4. NODE INSTALL"
-cd $HOME 
-rm -rf celestia-node 
-git clone https://github.com/celestiaorg/celestia-node.git 
-cd celestia-node/ 
-git checkout tags/v0.6.4 
-make build 
+cd $HOME
+rm -rf celestia-node
+git clone https://github.com/celestiaorg/celestia-node.git
+cd celestia-node/
+git checkout tags/v0.11.0-rc8
+make build
 make install
 
 echo "5. CEL-KEY MAKING"
@@ -43,7 +43,7 @@ Description=celestia-lightd Light Node
 After=network-online.target
 [Service]
 User=$USER
-ExecStart=$HOME/go/bin/celestia light start --core.ip $RPCIP --keyring.accname $NODENAME --p2p.network mocha
+ExecStart=$HOME/go/bin/celestia light start --core.ip $RPCIP --keyring.accname $NODENAME --p2p.network mocha-3
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=4096
@@ -55,3 +55,4 @@ echo "8. START NODE SERVICE"
 sudo systemctl enable celestia-lightd
 sudo systemctl start celestia-lightd
 sudo systemctl status celestia-lightd
+#journalctl -u celestia-lightd.service -f
