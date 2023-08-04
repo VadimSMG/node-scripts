e1fe914128ed60da6e4bb934668a8aa62c04da20
VadimSMG <106158663+VadimSMG@users.noreply.github.com>
2023-08-04 18:10:30 +0300

    Update namada-install.sh. Added full node setup

--- namada-install.sh
+++ namada-install.sh
@@ -17,3 +17,24 @@ git clone https://github.com/anoma/namada.git
 cd namada 
 make install
 
+echo "3. INSTALLING COMETBFT"
+echo export GOPATH=\"\$HOME/go\" >> ~/.bash_profile
+echo export PATH=\"\$PATH:\$GOPATH/bin\" >> ~/.bash_profile
+git clone https://github.com/cometbft/cometbft.git
+cd cometbft
+make install
+make build
+
+echo "4. DOWNLOADING BINARIES"
+OPERATING_SYSTEM="Linux" # or "Darwin" for MacOS
+latest_release_url=$(curl -s "https://api.github.com/repos/anoma/namada/releases/latest" | grep "browser_download_url" | cut -d '"' -f 4 | grep "$OPERATING_SYSTEM")
+wget "$latest_release_url"
+
+echo "5. FULL NODE SETUP"
+
+echo "5.1 Joining the network. Input actual chain-id:"
+read CHAIN_ID
+namada client utils join-network --chain-id $CHAIN_ID
+
+echo "5.2 Start node and sync"
+CMT_LOG_LEVEL=p2p:none,pex:error namada node ledger run
