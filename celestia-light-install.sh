18a49845e7cdec29bf4335fe2f39e3654935ed1f
VadimSMG <106158663+VadimSMG@users.noreply.github.com>
2023-09-07 17:47:15 +0300

    Update celestia-light-install.sh
    Change GO version

--- celestia-light-install.sh
+++ celestia-light-install.sh
@@ -6,7 +6,7 @@ sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential
 git make ncdu -y
 
 echo "3. GOLANG INSTALL"
-ver="1.20.2"
+ver="1.21.0"
 cd $HOME
 wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
