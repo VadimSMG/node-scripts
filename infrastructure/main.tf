237a56d2088988612b0ced47a4f356d29d9f90dd
VadimSMG <vadimsmg@gmail.com>
2024-08-21 08:13:04 +0300

    Create terraform infrastructure files.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,22 +1,25 @@
 terraform {
-  required_providers {
+required_providers {
     contabo = {
       source = "contabo/contabo"
-      version = "~> 1.0"
+      version = ">= 0.1.26"
     } 
   }
 }
 #Налаштування авторизації на ресурсі Contabo.
 provider "contabo" {
   #Використання облікових даних профілю на Contabo. Знаходяться у Contabo Control Panel.
-  oauth2_client_id = "" #ID акаунту. 
-  oauth2_client_secret = "" #Пароль акаунту.
-  oauth_user = "" #API пароль.
-  oauth_pass = "" #API користувача. Це емейл, на якиз раеєстровано профіль.
-  oauth2_token_url = "" #Токен oauth2. Отримати за адресою https://auth.contabo.com/auth/realms/contabo/protocol/openid-connect/token
+  oauth2_client_id = var.client_id #ID акаунту. 
+  oauth2_client_secret = var.client_secret #Пароль акаунту.
+  oauth2_user = var.api_user #API пароль.
+  oauth2_pass = var.api_password #API користувача. Це емейл, на якиз раеєстровано профіль.
+  oauth2_token_url = var.access_token #Токен oauth2. Отримати за адресою https://auth.contabo.com/auth/realms/contabo/protocol/openid-connect/token
 }
 #Налаштування параметрів імпортуємого серверу.
 resource "contabo_instance" "test_instance" {
-  dislay_name = "194.163.134.151"
-  product_id = ""
+  name = "vmi1063448"
+  #image_id = "afecbb85-e2fc-46f0-9684-b46b1faf00bb"
+  dislay_name = ""
+  product_id = "V36"
+  region = "EU"
 }
